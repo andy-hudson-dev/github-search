51bd1a86b60cfd7a123cfeb5a064b5b5cc382509
Andrew Hudson <Andy@andrews-imac.home>
2021-08-15 15:51:32 +0100

    search api tests and error handling

--- src/api/__tests__/search.tests.js
+++ src/api/__tests__/search.tests.js
@@ -2,6 +2,8 @@ import search from "../search";
 
 const defaultFetch = global.fetch;
 
+const MOCK_QUERY= "test";
+
 beforeEach(() => {
     global.fetch = jest.fn();
 
@@ -18,14 +20,77 @@ afterEach(() => {
 
 describe("search", () => {
     it("should search for users by default", async () => {
-        const mockQuery = "test";
-
-        await search(mockQuery);
+        await search(MOCK_QUERY);
 
         expect(expect(window.fetch).toHaveBeenCalledWith(
             expect.stringContaining(
-                `https://api.github.com/search/users?q=${mockQuery}`,
+                `https://api.github.com/search/users`,
             ),
         ));
+    });
+
+    it("should call api with passed in query", async () => {
+        await search(MOCK_QUERY);
+
+        expect(expect(window.fetch).toHaveBeenCalledWith(
+            expect.stringContaining(
+                `https://api.github.com/search/users?q=${MOCK_QUERY}`,
+            ),
+        ));
+    });
+
+    it("should request 10 records per page by default", async() => {
+        await search(MOCK_QUERY);
+
+        expect(expect(window.fetch).toHaveBeenCalledWith(
+            expect.stringContaining(
+                `https://api.github.com/search/users?q=${MOCK_QUERY}&per_page=10`,
+            ),
+        ));
+    });
+
+    it("should request page 1 by default", async() => {
+        await search(MOCK_QUERY);
+
+        expect(expect(window.fetch).toHaveBeenCalledWith(
+            expect.stringContaining(
+                `https://api.github.com/search/users?q=${MOCK_QUERY}&per_page=10&page=1`,
+            ),
+        ));
+    });
+    
+    it("should query api with passed in pagination and type parameters", async() => {
+        await search(MOCK_QUERY, { type: "random", recordsPerPage: 100, page: 4 });
+
+        expect(expect(window.fetch).toHaveBeenCalledWith(
+            expect.stringContaining(
+                `https://api.github.com/search/random?q=${MOCK_QUERY}&per_page=100&page=4`,
+            ),
+        ));
+    });
+
+    it("should throw if no query is passed", async () => {
+        try {
+            await search();
+        } catch (error) {
+            expect(error.message).toBe("Please provide a search term.");
+        }
+    });
+
+    it("should throw if api returns unsuccessful", async () => {
+        global.fetch.mockImplementation(() =>
+            Promise.resolve({
+                status: 500,
+            }),
+        );
+
+        try {
+            await search(MOCK_QUERY);
+        } catch (error) {
+            /* eslint-disable-next-line jest/no-conditional-expect */
+            expect(error.message).toBe(
+                "An unknown error has occurred",
+            );
+        }
     })
 })
--- src/api/search.js
+++ src/api/search.js
@@ -2,10 +2,19 @@ async function search(
     query,
     { type = "users", recordsPerPage = 10, page = 1 } = {},
 ) {
+    if (!query || query === "") {
+        throw new Error("Please provide a search term.");
+    }
 
     const response = await fetch(
         `https://api.github.com/search/${type}?q=${query}&per_page=${recordsPerPage}&page=${page}`,
     );
+
+    if (!response.json) {
+        throw new Error(
+            "An unknown error has occurred",
+        );
+    }
     
     const data = await response.json();
 
